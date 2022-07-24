039c1260788836707afa4f9acb9bb4ea38f69ba8
Gopi M <gopi.learner93@gmail.com>
2022-07-24 12:09:22 +0530

    valid panlindrome add

---
+++ valid_palindrome/two-pointer.js
@@ -0,0 +1,16 @@
+let isPalindrome = function (s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
